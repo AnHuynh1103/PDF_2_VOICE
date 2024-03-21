111dec46d79ae9179390aad05318ed4dc593b512
An <106163753+AnHuynh1103@users.noreply.github.com>
2024-03-21 14:14:14 +0700

    Add files via upload

---
+++ pdf2text.py
@@ -0,0 +1,17 @@
+# import module  
+import PyPDF2 
+
+# path of PDF 
+path = open('file.pdf', 'rb') 
+
+# creat a PdfReader object 
+pdfReader = PyPDF2.PdfReader(path) 
+
+# start at the 4th page  
+# read the page. 
+from_page = pdfReader.pages[4] 
+
+# extracting the text from the PDF 
+text = from_page.extract_text() 
+
+print(text)
